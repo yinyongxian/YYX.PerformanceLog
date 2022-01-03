4e2d811af21e893279cb06b7fa0e6591fc9b422f
yinyongxian <yinyongxian@qq.com>
2022-01-03 22:55:03 +0800

    Add project files.

---
+++ YYX.PerformanceLog/Loger.cs
@@ -0,0 +1,60 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YYX.PerformanceLog
+{
+    public class Loger : IDisposable
+    {
+        private bool disposedValue;
+        private readonly ILog log;
+        private string message;
+        private DateTime startDatetime;
+
+        public Loger(string message = "")
+        {
+            this.message = message;
+            startDatetime = DateTime.Now;
+
+            this.log = LogManager.GetLogger("");
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects)
+                    var endDateTime = DateTime.Now;
+                    message += Environment.NewLine + startDatetime.ToLongDateString();
+                    message += Environment.NewLine + endDateTime.ToLongDateString();
+                    message += Environment.NewLine + (endDateTime - startDatetime);
+                    message += Environment.NewLine;
+                    log.Debug(message);
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+                // TODO: set large fields to null
+                disposedValue = true;
+            }
+        }
+
+        // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+        // ~Loger()
+        // {
+        //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        //     Dispose(disposing: false);
+        // }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ YYX.PerformanceLog/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using YYX.PerformanceLog;
+using Loger loger = new Loger();
+Foo();
+Console.WriteLine("YYX");
+
+void Foo()
+{
+    Thread.Sleep(1000);
+}
